28372a828935fa2300fbbc46a10349154173bc98
David Stevenson <drstevenson1958@yahoo.com>
2023-08-02 11:19:31 -0400

    Listing 3-1. Adding an Action Method to the PageSizeController.cs File

---
+++ ch03/Primer/Controllers/PageSizeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Primer.Controllers
+{
+    public class PageSizeController : Controller
+    {
+        private static string TargetUrl = "http://apress.com";
+
+        public long GetPageSize()
+		{
+            WebClient wc = new WebClient();
+            Stopwatch sw = Stopwatch.StartNew();
+            byte[] apressData = wc.DownloadData(TargetUrl);
+            Debug.WriteLine($"Elapsed ms: {sw.ElapsedMilliseconds} milliseconds.");
+            return apressData.LongLength;
+		}
+    }
+}
