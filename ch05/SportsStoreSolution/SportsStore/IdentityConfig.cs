8510a467338b744b6f8f44478f38cb755f58c074
David Stevenson <drstevenson1958@yahoo.com>
2023-08-03 12:50:57 -0400

    Creating the OWIN Startup Class
    Listing 5-3. The Contents of the IdentityConfig.cs file

---
+++ ch05/SportsStoreSolution/SportsStore/IdentityConfig.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+//[assembly: OwinStartup(SportsStore.IdentityConfig)]
+
+namespace SportsStore
+{
+
+	public class IdentityConfig
+	{
+		public void Configuration(IAppBuilder app) { }
+	}
+}
+
