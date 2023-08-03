d141991db71df81df32f951712a821b1ebe729bf
David Stevenson <drstevenson1958@yahoo.com>
2023-08-03 12:41:10 -0400

    Listing 5-1. The Contents of the PrepController.cs File
    Fixed Newtonsoft.Json package compiler warnings with newer package version.

---
+++ ch05/SportsStoreSolution/SportsStore/Controllers/PrepController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace SportsStore.Controllers
+{
+	public class PrepController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
