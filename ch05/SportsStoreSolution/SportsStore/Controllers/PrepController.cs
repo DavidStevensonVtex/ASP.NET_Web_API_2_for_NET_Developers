a0c225c15a631e7296aa9a2a5eef069f5b65d01f
David Stevenson <drstevenson1958@yahoo.com>
2023-08-04 14:23:05 -0400

    Listing 5-20. Adding Action Methods and Filters in the PrepController.cs file

--- ch05/SportsStoreSolution/SportsStore/Controllers/PrepController.cs
+++ ch05/SportsStoreSolution/SportsStore/Controllers/PrepController.cs
@@ -1,6 +1,12 @@
-﻿using SportsStore.Models;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using System.Web.Mvc;
+using Microsoft.Owin.Security;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System.Web;
+using System.Security.Claims;
+using SportsStore.Models;
+using SportsStore.Infrastructure.Identity;
 
 namespace SportsStore.Controllers
 {
@@ -18,12 +24,14 @@ namespace SportsStore.Controllers
             return View(repo.Products);
         }
 
+        [Authorize(Roles = "Administrators")]
         public async Task<ActionResult> DeleteProduct(int id)
 		{
             await repo.DeleteProductAsync(id);
             return RedirectToAction("Index");
         }
 
+        [Authorize(Roles = "Administrators")]
         public async Task<ActionResult> SaveProduct(Product product)
 		{
             await repo.SaveProductAsync(product);
@@ -46,5 +54,21 @@ namespace SportsStore.Controllers
             await repo.SaveOrderAsync(order);
             return RedirectToAction("Orders");
 		}
+
+        public async Task<ActionResult> SignIn ()
+        {
+            IAuthenticationManager authMgr = HttpContext.GetOwinContext().Authentication;
+            StoreUserManager userMgr = HttpContext.GetOwinContext().GetUserManager<StoreUserManager>();
+
+            StoreUser user = await userMgr.FindAsync("Admin", "secret");
+            authMgr.SignIn(await userMgr.CreateIdentityAsync(user, DefaultAuthenticationTypes.ApplicationCookie));
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult SignOut()
+        {
+            HttpContext.GetOwinContext().Authentication.SignOut();
+            return RedirectToAction("Index");
+        }
     }
 }
