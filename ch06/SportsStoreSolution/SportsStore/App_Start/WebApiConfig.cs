1b5a06a284264dbc14f8d6098ca8361b6e68e310
David Stevenson <drstevenson1958@yahoo.com>
2023-08-08 15:20:49 -0400

    Listing 6-14. Defining a New Route in the WebApiConfig.cs file

--- ch06/SportsStoreSolution/SportsStore/App_Start/WebApiConfig.cs
+++ ch06/SportsStoreSolution/SportsStore/App_Start/WebApiConfig.cs
@@ -10,10 +10,12 @@ namespace SportsStore
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
-
-            // Web API routes
             config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "OrdersRoute",
+                routeTemplate: "nonrest/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional });
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
