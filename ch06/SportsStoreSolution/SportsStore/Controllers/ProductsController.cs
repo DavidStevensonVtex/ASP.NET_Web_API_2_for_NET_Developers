6d9a095cf4f15fe3da277f16f2c8f3ae3c518f99
David Stevenson <drstevenson1958@yahoo.com>
2023-08-08 13:48:19 -0400

    Listing 6-2. Adding RESTful Action Methods in the ProductsController.cs File

--- ch06/SportsStoreSolution/SportsStore/Controllers/ProductsController.cs
+++ ch06/SportsStoreSolution/SportsStore/Controllers/ProductsController.cs
@@ -4,10 +4,36 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using SportsStore.Models;
+using System.Threading.Tasks;
 
 namespace SportsStore.Controllers
 {
     public class ProductsController : ApiController
     {
+        private IRepository Repository { get; set; }
+        public ProductsController()
+		{
+            Repository = new ProductRepository();
+		}
+
+        public IEnumerable<Product> GetProducts()
+		{
+            return Repository.Products;
+		}
+        public Product GetProduct ( int id)
+		{
+            return Repository.Products.FirstOrDefault(p => p.Id == id);
+		}
+
+        public async Task PostProduct(Product product)
+		{
+            await Repository.SaveProductAsync(product);
+		}
+
+        public async Task DeleteProduct(int id)
+		{
+            await Repository.DeleteProductAsync(id);
+		}
     }
 }
